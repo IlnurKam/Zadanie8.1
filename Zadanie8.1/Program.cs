398fb6267eac2eddb3ce9b648fc600adef6cc5df
kamal673 <kamal673@yandex.ru>
2022-01-20 22:22:50 +0300

    Добавьте файлы проекта.

---
+++ Zadanie8.1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace HomeWork8._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo dir = new DirectoryInfo(@"C:\Users\Илнур\Documents\Уку");
+            
+            foreach (var item in dir.GetDirectories())
+            {
+                Console.WriteLine(item.Name);
+                Console.WriteLine("-Список подкаталогов-");
+                foreach (var it in item.GetDirectories())
+                    Console.WriteLine(it.Name);
+                Console.WriteLine();
+            }
+            Console.WriteLine("-Список файлов-");
+            foreach (var item in dir.GetFiles())
+            {
+                Console.WriteLine(item.Name);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Zadanie8.2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace HomeWork8._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter f1 = new StreamWriter(@"G:\Автоматизация BIM проектирования\Задание 8.2\f1.txt");
+            Random r = new Random();
+            for (int i = 0; i < 10; i++) f1.WriteLine(r.Next(0, 100));
+            f1.Close();
+            StreamReader fr = new StreamReader(@"G:\Автоматизация BIM проектирования\Задание 8.2\f1.txt");
+            string s = "";
+            int max = Int32.Parse(fr.ReadLine()), num = 0;
+            do
+            {
+                s = fr.ReadLine();
+                if (int.TryParse(s, out num)) max = max < num ? num : max;
+            } while (s != null);
+            fr.Close();
+            StreamWriter f2 = new StreamWriter(@"G:\Автоматизация BIM проектирования\Задание 8.2\f2.txt");
+            f2.WriteLine(max);
+            f2.Close();
+            {
+                string[] lines = File.ReadAllLines(@"G:\Автоматизация BIM проектирования\Задание 8.2\f2.txt"); 
+                foreach (var line in lines)
+                    Console.WriteLine(line);
+            }
+        }
+    }
+}
